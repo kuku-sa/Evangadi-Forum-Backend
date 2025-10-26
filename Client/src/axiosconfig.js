de187c2a532c7a9248db771a58cafb018fb07012
Rahel Mesfin <rmesfin50@gmail.com>
2025-10-26 21:30:43 +0300

    Fix axiosconfig to use correct API URL

--- Client/src/axiosconfig.js
+++ Client/src/axiosconfig.js
@@ -1,15 +1,1 @@
-// import axios from 'axios'
-// const axiosInstance =axios.create({
-//     baseURL:'http://localhost:5000/api',
-//     //deployed version of evangadi server on render.com 
-//    baseURL: "https://new-evangadiforum-backend.onrender.com"
-// });
-// export default axiosInstance;
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-  withCredentials: true,
-});
-
-export default axiosInstance;
+import axios from "axios"; import { API_URL } from "./config"; const axiosInstance = axios.create({ baseURL: API_URL, withCredentials: true, }); export default axiosInstance;
